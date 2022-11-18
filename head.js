fac87a5a437feafa04945b0a6c5c3aeec1c0e9e6
thahira97 <thahirasheerin3@gmail.com>
2022-11-17 20:52:29 -0500

    Add head function

--- head.js
+++ head.js
@@ -5,6 +5,10 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(array) {
+  return array[0];
+};
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
