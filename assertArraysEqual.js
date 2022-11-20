00355b2ba90e3ffd57dd89620fb05a86f61b3eeb
thahira97 <thahirasheerin3@gmail.com>
2022-11-19 19:10:17 -0500

    Add assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  
+  if (eqArrays(array1,array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+console.log(assertArraysEqual([8,7,6],[8,7,6]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
