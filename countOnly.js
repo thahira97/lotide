0d8e9e6043495a8aba89ae5bb06b0340fee56fc5
thahira97 <thahirasheerin3@gmail.com>
2022-11-22 11:03:16 -0500

    modify in code

--- countOnly.js
+++ countOnly.js
@@ -9,7 +9,6 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount){
  const results = {}
  for (const item of allItems) {
-  console.log(item);
   if (itemsToCount[item]) { 
     if (results[item]) {
       results[item] += 1;
