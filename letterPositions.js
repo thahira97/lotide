88ba7cc007f6a8193f57abe6c11fae5abbe5044c
thahira97 <thahirasheerin3@gmail.com>
2022-11-22 17:10:20 -0500

    check output calling assertequal function

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,23 @@
-// const eqArrays = function(array1, array2) {
-//   if (array1.length !== array2.length) {
-//     return false;
-//   } else {
-//     for (let i = 0; i < array1.length; i++) {
-//       if (array1[i] !== array2[i]) {
-//         return false;
-//       }
-//     }
-//   }
-//   return true;
-// };
-// const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
   
-//   if (eqArrays(array1,array2)) {
-//     console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-//   } else {
-//     console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
-//   }
-// };
+  if (eqArrays(array1,array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
 const letterPositions = function(sentence) {
   const result = {};
   for (let i in sentence) {
@@ -30,6 +30,6 @@ const letterPositions = function(sentence) {
 };
 
 
-console.log(letterPositions("hello"))
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
 
 
