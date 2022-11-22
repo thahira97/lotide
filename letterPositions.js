33db55fc7488e8b0f18472f683b0542f78389ebc
thahira97 <thahirasheerin3@gmail.com>
2022-11-22 17:04:58 -0500

    Add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+// const eqArrays = function(array1, array2) {
+//   if (array1.length !== array2.length) {
+//     return false;
+//   } else {
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] !== array2[i]) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
+// const assertArraysEqual = function(array1, array2) {
+  
+//   if (eqArrays(array1,array2)) {
+//     console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+//   } else {
+//     console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+//   }
+// };
+const letterPositions = function(sentence) {
+  const result = {};
+  for (let i in sentence) {
+    if(sentence[i] !== ' ') {
+    if (result[sentence[i]]) {
+      result[sentence[i]].push(Number(i));
+    } else result[sentence[i]] = [Number(i)]
+  } 
+} return result;
+};
+
+
+console.log(letterPositions("hello"))
+
+
