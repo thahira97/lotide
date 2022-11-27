5f98e2c168d209c6affa5070ca8d2d6a2d61ebe8
thahira97 <thahirasheerin3@gmail.com>
2022-11-26 19:02:52 -0500

    Add takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,1 +1,16 @@
  
+ 
+  const array = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+ const takeUntil = function (array,callback) {
+   const result= [];
+   for(let arr of array){
+    if (!callback(arr)){
+      result.push(arr)
+    } else{
+      return result
+    }
+   }
+ };
+ const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+ const results1 = takeUntil(data1, x => x < 0);
+ console.log(results1);
