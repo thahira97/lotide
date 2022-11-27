8f29038587637eb273a7c86fdc9cf9ee24a8c26f
thahira97 <thahirasheerin3@gmail.com>
2022-11-26 19:31:19 -0500

    Add findKey.js

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const findKey = function(obj, callback) {
+  const objKey = Object.keys(obj);
+  for (const val of objKey) {
+    if (callback(obj[val])) {
+      return val;
+    }
+  }
+};
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
